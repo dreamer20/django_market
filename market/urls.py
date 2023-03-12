4c1d0a455b9c56ed4c4b0461eeb3be4404b65a40
dreamer20 <gv4alex@gmail.com>
2023-03-12 17:51:41 +0300

    Add slash to paths

--- market/urls.py
+++ market/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('category/<str:category>', views.CategoryView.as_view(), name='category'),
-    path('cart', views.CartView.as_view(), name='cart'),
-    path('cart/remove', views.CartRemoveProductView.as_view(), name='cart_remove'),
-    path('cart/add', views.CartAddProductView.as_view(), name='cart_add'),
-    path('cart/reduce', views.CartReduceProductCountView.as_view(), name='cart_reduce'),
-    path('cart/order', views.OrderView.as_view(), name='order'),
+    path('cart/', views.CartView.as_view(), name='cart'),
+    path('cart/remove/', views.CartRemoveProductView.as_view(), name='cart_remove'),
+    path('cart/add/', views.CartAddProductView.as_view(), name='cart_add'),
+    path('cart/reduce/', views.CartReduceProductCountView.as_view(), name='cart_reduce'),
+    path('cart/order/', views.OrderView.as_view(), name='order'),
 ]
