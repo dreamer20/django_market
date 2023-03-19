538f282489dfd9d456b5bf754c873d7944442b42
dreamer20 <gv4alex@gmail.com>
2023-03-19 19:44:38 +0300

    Forms refactoring.

--- market/forms.py
+++ market/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Laptop, Order, Desktop, Television
-from .utils import getFieldValueList
+
+
+def getChoices(Model, field_name):
+    return lambda: Model.objects.values_list(field_name, field_name).distinct()
 
 
 class MyWidjet(forms.CheckboxSelectMultiple):
@@ -33,12 +36,6 @@ class OrderForm(forms.ModelForm):
 
 class LaptopFilterForm(forms.Form):
     common_attrs = {'class': 'form-control'}
-    CPU_LIST = getFieldValueList(Laptop, 'cpu')
-    GPU_LIST = getFieldValueList(Laptop, 'gpu')
-    RAM_MEMORY_LIST = getFieldValueList(Laptop, 'ram_memory')
-    SCREEN_SIZE_LIST = getFieldValueList(Laptop, 'screen_size')
-    DISK_SIZE_LIST = getFieldValueList(Laptop, 'disk_size')
-    OS_LIST = getFieldValueList(Laptop, 'os')
 
     price_min = forms.IntegerField(
         min_value=0,
@@ -53,42 +50,42 @@ class LaptopFilterForm(forms.Form):
     cpu = forms.MultipleChoiceField(
 
         label='Processor',
-        choices=CPU_LIST,
+        choices=getChoices(Laptop, 'cpu'),
         required=False,
         widget=MyWidjet()
     )
 
     gpu = forms.MultipleChoiceField(
         label='GPU',
-        choices=GPU_LIST,
+        choices=getChoices(Laptop, 'gpu'),
         required=False,
         widget=MyWidjet()
     )
 
     ram_memory = forms.MultipleChoiceField(
         label='Ram memory, Gb',
-        choices=RAM_MEMORY_LIST,
+        choices=getChoices(Laptop, 'ram_memory'),
         required=False,
         widget=MyWidjet()
     )
 
     screen_size = forms.MultipleChoiceField(
         label='Screen size',
-        choices=SCREEN_SIZE_LIST,
+        choices=getChoices(Laptop, 'screen_size'),
         required=False,
         widget=MyWidjet()
     )
 
     disk_size = forms.MultipleChoiceField(
         label='Disk size, Gb',
-        choices=DISK_SIZE_LIST,
+        choices=getChoices(Laptop, 'disk_size'),
         required=False,
         widget=MyWidjet()
     )
 
     os = forms.MultipleChoiceField(
         label='OS',
-        choices=OS_LIST,
+        choices=getChoices(Laptop, 'os'),
         required=False,
         widget=MyWidjet()
     )
@@ -96,43 +93,40 @@ class LaptopFilterForm(forms.Form):
 
 class DesktopFilterForm(forms.Form):
     common_attrs = {'class': 'form-control'}
-    CPU_LIST = getFieldValueList(Desktop, 'cpu')
-    GPU_LIST = getFieldValueList(Desktop, 'gpu')
-    DISK_SIZE_LIST = getFieldValueList(Desktop, 'disk_size')
-    OS_LIST = getFieldValueList(Desktop, 'os')
-    # RAM_MEMORY_LIST = getFieldValueList(Desktop, 'ram_memory')
 
     price_min = forms.IntegerField(
         min_value=0,
         widget=forms.NumberInput(common_attrs),
         required=False
     )
+
     price_max = forms.IntegerField(
         min_value=0,
         widget=forms.NumberInput(common_attrs),
         required=False
     )
+
     cpu = forms.MultipleChoiceField(
         label='Processor',
-        choices=CPU_LIST,
+        choices=getChoices(Desktop, 'cpu'),
         required=False,
         widget=MyWidjet()
     )
     gpu = forms.MultipleChoiceField(
         label='GPU',
-        choices=GPU_LIST,
+        choices=getChoices(Desktop, 'gpu'),
         required=False,
         widget=MyWidjet()
     )
     disk_size = forms.MultipleChoiceField(
         label='Disk size',
-        choices=DISK_SIZE_LIST,
+        choices=getChoices(Desktop, 'disk_size'),
         required=False,
         widget=MyWidjet()
     )
     os = forms.MultipleChoiceField(
         label='OS',
-        choices=OS_LIST,
+        choices=getChoices(Desktop, 'os'),
         required=False,
         widget=MyWidjet()
     )
@@ -140,11 +134,6 @@ class DesktopFilterForm(forms.Form):
 
 class TelevisionFilterForm(forms.Form):
     common_attrs = {'class': 'form-control'}
-    SCREEN_SIZE_LIST = getFieldValueList(Television, 'screen_size')
-    RESOLUTION_LIST = getFieldValueList(Television, 'resolution')
-    DISPLAY_TECHNOLOGY_LIST = getFieldValueList(Television, 'display_technology')
-    CONNECTIVITY_TECHNOLOGY_LIST = getFieldValueList(Television, 'connectivity_technology')
-    PRODUCT_DIMENSIONS_LIST = getFieldValueList(Television, 'product_dimensions')
 
     price_min = forms.IntegerField(
         min_value=0,
@@ -158,19 +147,19 @@ class TelevisionFilterForm(forms.Form):
     )
     screen_size = forms.MultipleChoiceField(
         label='Screen size',
-        choices=SCREEN_SIZE_LIST,
+        choices=getChoices(Television, 'screen_size'),
         required=False,
         widget=MyWidjet()
     )
     resolution = forms.MultipleChoiceField(
         label='Resolution',
-        choices=RESOLUTION_LIST,
+        choices=getChoices(Television, 'resolution'),
         required=False,
         widget=MyWidjet()
     )
     display_technology = forms.MultipleChoiceField(
         label='Display technology',
-        choices=DISPLAY_TECHNOLOGY_LIST,
+        choices=getChoices(Television, 'display_technology'),
         required=False,
         widget=MyWidjet()
     )
